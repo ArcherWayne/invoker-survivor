fca81a794008e590f8bf6d921d6d4413c7f6451d
Archer Wayne <archerwayne1216@gmail.com>
2023-09-03 23:50:46 +0800

    improve orb function
    Signed-off-by: Archer Wayne <archerwayne1216@gmail.com>

--- scenes/characters/invoker.cs
+++ scenes/characters/invoker.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
 
 
@@ -26,14 +27,32 @@ public partial class invoker : CharacterBody2D
 	// private bool isFacingLeft = false;
 
 	// orb related parameters
-	private PackedScene OrbsScene;
+	// private PackedScene OrbsScene;
 	// private Node2D OrbsStartPosition;
 	private Node2D HadOrbs;
 	private Marker2D OrbsPos1;
 	private Marker2D OrbsPos2;
 	private Marker2D OrbsPos3;
 
-	private string[] OrbSlots = new string[3];
+	private Node2D Orbs1;
+	private Node2D Orbs2;
+	private Node2D Orbs3;
+
+	[Export]
+	public float OrbsDistanceWithPlayer = 40.0f;
+	[Export]
+	public float OrbsRotationAngularSpeed = 1.0f;
+	private float OrbsCurrentAngle = 0.0f;
+
+	[Signal]
+	public delegate void Orb1SetTypeEventHandler(string type);
+	[Signal]
+	public delegate void Orb2SetTypeEventHandler(string type);
+	[Signal]
+	public delegate void Orb3SetTypeEventHandler(string type);
+
+	private int OrbsSlotsMaxLenght = 3;
+	private string [] OrbsSlots = new string[3] {"Quas", "Wex", "Exort"};
 
 	public override void _Ready()
 	{
@@ -46,17 +65,21 @@ public partial class invoker : CharacterBody2D
 
 		HadOrbs = (Node2D)GetNode("HadOrbs");
 
-		// OrbsStartPosition = (Node2D)GetNode("OrbsStartPosition");
+		Orbs1 = (Node2D)GetNode("HadOrbs/Orbs1");
+		Orbs2 = (Node2D)GetNode("HadOrbs/Orbs2");
+		Orbs3 = (Node2D)GetNode("HadOrbs/Orbs3");
 
-		OrbsScene = (PackedScene)ResourceLoader.Load("res://scenes/characters/orbs.tscn");
-
-
+		// OrbsDistanceWithPlayer = 40;
+		// OrbsRotationAngularSpeed = 30;
 	}
 	public override void _Process(double delta)
 	{
 		ChangeFacingDirection();
 		GetOrbs();
+		SetOrbsType();
 
+		AdjustMarkPosition(delta);
+		AdjustOrbsPosition();
 		/*** debug section
 
 
@@ -141,26 +164,67 @@ public partial class invoker : CharacterBody2D
 	{
 		if (Input.IsActionJustPressed("Quas"))
 		{
-			GD.Print("q");
-			Node2D Orbs = (Node2D)OrbsScene.Instantiate();
-			Orbs.Position = OrbsPos1.Position;
-			HadOrbs.AddChild(Orbs);
+			int i = 0;
+			for (i = 0; i<2; i++)
+			{
+				OrbsSlots[i] = OrbsSlots[i + 1];
+			}
+			OrbsSlots[2] = "Quas";
 		}
 
 		if (Input.IsActionJustPressed("Wex"))
 		{
-			GD.Print("w");
-			Node2D Orbs = (Node2D)OrbsScene.Instantiate();
-			Orbs.Position = OrbsPos2.Position;
-			HadOrbs.AddChild(Orbs);
+			int i = 0;
+			for (i = 0; i<2; i++)
+			{
+				OrbsSlots[i] = OrbsSlots[i + 1];
+			}
+			OrbsSlots[2] = "Wex";
 		}
 
 		if (Input.IsActionJustPressed("Exort"))
 		{
-			GD.Print("e");
-			Node2D Orbs = (Node2D)OrbsScene.Instantiate();
-			Orbs.Position = OrbsPos3.Position;
-			HadOrbs.AddChild(Orbs);
+			int i = 0;
+			for (i = 0; i<2; i++)
+			{
+				OrbsSlots[i] = OrbsSlots[i + 1];
+			}
+			OrbsSlots[2] = "Exort";
 		}
+		GD.Print(OrbsSlots);
 	}
+
+	private void SetOrbsType()
+	{
+		EmitSignal(SignalName.Orb1SetType, OrbsSlots[0]);
+		EmitSignal(SignalName.Orb2SetType, OrbsSlots[1]);
+		EmitSignal(SignalName.Orb3SetType, OrbsSlots[2]);
+	}
+
+	private void AdjustMarkPosition(double delta)
+	{
+
+		float deg120 = 2 * (Mathf.Pi / 3);
+		OrbsCurrentAngle += OrbsRotationAngularSpeed * (float)delta;
+
+		float x_1 = OrbsDistanceWithPlayer * Mathf.Cos(OrbsCurrentAngle);
+		float y_1 = OrbsDistanceWithPlayer * Mathf.Sin(OrbsCurrentAngle);
+		float x_2 = OrbsDistanceWithPlayer * Mathf.Cos(OrbsCurrentAngle+deg120);
+		float y_2 = OrbsDistanceWithPlayer * Mathf.Sin(OrbsCurrentAngle+deg120);
+		float x_3 = OrbsDistanceWithPlayer * Mathf.Cos(OrbsCurrentAngle-deg120);
+		float y_3 = OrbsDistanceWithPlayer * Mathf.Sin(OrbsCurrentAngle-deg120);
+
+		OrbsPos1.Position = new Vector2(x_1, y_1);
+		OrbsPos2.Position = new Vector2(x_2, y_2);
+		OrbsPos3.Position = new Vector2(x_3, y_3);
+		
+	}
+
+	private void AdjustOrbsPosition()
+	{
+		Orbs1.Position = OrbsPos1.Position;
+		Orbs2.Position = OrbsPos2.Position;
+		Orbs3.Position = OrbsPos3.Position;
+	}
+
 }
--- scenes/characters/orbs.cs
+++ scenes/characters/orbs.cs
@@ -6,11 +6,24 @@ public partial class orbs : Node2D
 
 
 	[Export]
-	public string type;
+	public string type { get; set; }
+	// {
+	// 	get
+	// 	{
+	// 		return type;
+	// 	}
+	// 	set
+	// 	{
+	// 		type = value;
+	// 	}
+	// }
+	// public string AccessType
+
 
 	public string[] orbsTypeArray = new string[3];
 
 	private Node2D OrbsImage;
+
 	
 	public override void _Ready()
 	{
@@ -31,35 +44,29 @@ public partial class orbs : Node2D
 		SetOrbColor();
 	}
 
-	public void SetOrbType(string orbType)
-	{
-		if (Array.IndexOf(orbsTypeArray, orbType) != -1)
-		{
-			type = orbType;
-		}
-		else
-		{
-
-		}
-	}
 
 	public void SetOrbColor()
 	{
 		switch(type){
 			case "Quas" :
-				OrbsImage.Modulate = new Color(0, 0, 1);
+				OrbsImage.Modulate = new Color(0.314f, 0.592f, 0.765f);
 				break;
 			case "Wex" :
-				OrbsImage.Modulate = new Color(0, 1, 0);
+				OrbsImage.Modulate = new Color(1f, 0.686f, 1f);
 				break;
 			case "Exort" :
-				OrbsImage.Modulate = new Color(1, 0, 0);
+				OrbsImage.Modulate = new Color(0.992f, 0.588f, 0.22f);
 				break;
 
 			default :
 				OrbsImage.Modulate = new Color(0, 0, 0);
 				break;
 		}
+	}
+
+	public void _on_invoker_orb_1_set_type(string AssignedType)
+	{
+		type = AssignedType;
 	}
 
 
